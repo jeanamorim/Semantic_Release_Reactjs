462264041532c78e06ae7ef74955ff7479682b1c
jeanamorim <jeanamorim08@hotmail.com>
2023-05-29 12:49:31 -0300

    fix: correções do semantic

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count} teste na pratica do semantic release
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code>ande & and and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
