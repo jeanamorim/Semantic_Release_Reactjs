9667f961a708b2996797601378eaf22784f2cef1
jeanamorim <jeanamorim08@hotmail.com>
2023-05-29 12:47:34 -0300

    fix: add semantic release no git

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} aqui é um teste de semantic release
+          count is {count} teste na pratica do semantic release
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
