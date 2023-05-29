356374553796635ce0354a2a817c085bd8d78312
jeanamorim <jeanamorim08@hotmail.com>
2023-05-29 13:00:24 -0300

    feat: Add Semantic Release and Github Actions

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite +ddd React</h1>
+      <h1>Vite +ddd Redddact</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count} teste d na pratica do semantic release
